85a7063c603a0db84217f9d438499649675f399d
inqu0302 <inqu0302@naver.com>
2021-06-29 15:15:27 +0900

    06-29-api사용법

--- SpMVC_040_book/src/main/java/com/callor/book/service/NaverService.java
+++ SpMVC_040_book/src/main/java/com/callor/book/service/NaverService.java
@@ -1,49 +1,36 @@
 package com.callor.book.service;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 
-import org.springframework.stereotype.Service;
+import org.json.simple.parser.ParseException;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Service
-public class NaverService {
+/*
+ * Naver의 OpenAPI를 통하여 데이터를 검색하는 서비스
+ * 도서정보, 뉴스정보, 영화정보를 가져오는데
+ * Naver에서 제공하는 데이터가 조금씩 차이가 난다
+ * 
+ * OpneAPI를 통해서 데이터를 가져올때 받을 데이터에 대한
+ * VO(DTO)를 생성해야 한다.
+ * 
+ * Naver에 받을 데이터에 대한 VO를 만들어야 하는데 각각 도서, 뉴스, 영화 데이터가
+ * 조금씩 달라서 3개의 VO를 생성해야 한다.
+ * 
+ * 각각 VO별로 서비스를 생성하기 위하여 미리 인터페이스를 만드는데 VO구조가 달라
+ * 다소 어려움이 있을것으로 예상된다
+ * 
+ * 그래서 인터페이스에 Generic을 선언하여 VO별로 필요에 따라 클래스를 만들수 있도록 한다
+ */
+public interface NaverService <T>{
 	
-	/*
-	 * naver에 요청하기
-	 * BookURL + "?query=" + 검색문자열
-	 */
-	protected final String BookURL = "https://openapi.naver.com/v1/search/book.json";
-	protected final String NewsURL = "https://openapi.naver.com/v1/search/news.json";
-	protected final String MovieURL = "https://openapi.naver.com/v1/search/movie.json";
-
-	public String queryURL(String search) {
-		
-		String serachUTF8 = null;
-		
-		// 검색하고자 하는 문자열을 UTF-8로 인코딩
-		try {
-			
-			serachUTF8 = URLEncoder.encode(search, "UTF-8");
-			
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		StringBuilder queryURL = new StringBuilder();
-		queryURL.append(BookURL); // queryString += BookURL
-		
-		String queryString = String.format("?query=%s", serachUTF8);
-		queryURL.append(queryString);
-		
-		queryString = String.format("&display=%d", 20);
-		
-		queryURL.append(queryString);
-		
-		return queryURL.toString();
-	}
+	// 검색문자열을 받아서 검색을 위한 URL을 생성하여 return
+	public String queryURL(String search_text);
 	
+	// queryURL을 Naver에 보내고 Naver에서 응답받은 데이터를 JSON형태의 문자열로 변환
+	public String getJsonString(String queryURL) throws MalformedURLException, IOException;
+	
+	// json 형태의 문자열을 받아서 VO를 담은 List type으로 리턴
+	// JSON 문자열을 parsing하여 객체(리스트) type으로 변환
+	public List<T> getNaverList(String jsonString) throws ParseException;
 }
--- SpMVC_040_book/src/main/webapp/WEB-INF/views/home.jsp
+++ SpMVC_040_book/src/main/webapp/WEB-INF/views/home.jsp
@@ -5,10 +5,101 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+*{
+	box-sizing: border-box;
+	margin:0;
+	padding:0;
+}
+
+p b{
+	color: blue;
+}
+
+nav#main_nav {
+	background-color: rgba(0,255,0,0.7);
+	display: flex;
+	justify-content: center;
+	align-items: center; 
+}
+
+nav#main_nav form{
+	margin:0.6rem;
+	width: 50%;
+}
+
+nav#main_nav input{
+	padding: 8px;
+	border: 0;
+	outline: 0;
+	width: 100%;
+	border-radius: 10px;
+}
+
+section.content_box{
+	border:1px solid green;
+	padding: 12px 16px;
+	display: flex;
+	flex-wrap: wrap;
+}
+
+section.content_box div.content{
+	border: 1px solid blue;
+	margin: 5px auto;
+	display: flex;
+	max-width: 30%;
+	height: 30vh;
+	overflow: auto;
+}
+
+section.content_box div.content img{
+	flex: 1;
+}
+
+section.content_box div.content div{
+	flex: 2;
+}
+
+@media (max-width:1000px){
+	section.content_box div.content{
+		width: 20%;
+	}
+}
+
+a {
+	text-decoration: none;
+}
+
+a:hover{
+	color: green;
+}
+</style>
 <meta charset="UTF-8">
-<title>내 도서관</title>
+<title>나의 홈페이지</title>
 </head>
 <body>
-	<h1>내 도서관</h1>
+	<nav id="main_nav">
+		<form>
+			<input name="search" placeholder="도서명을 입력후 Enter">
+		</form>
+	</nav>
+	<section class="content_box">
+		<c:forEach items="${BOOKS}" var="BOOKS">
+			<div class="content">
+				<img src="${BOOKS.image}">
+				<div>
+					<p class="title">
+						<a href="${BOOKS.link}" target="_NEW">
+							${BOOKS.title}
+						</a>
+					</p>
+					<p class="desc">${BOOKS.description}</p>
+					<p class="author"><strong>저자</strong>${BOOKS.author}</p>
+					<p class="publisher"><strong>출판사 : </strong>${BOOKS.publisher}</p>
+					<button class="insert">내 서재등록</button>
+				</div>
+			</div>
+		</c:forEach>
+	</section>	
 </body>
 </html>
